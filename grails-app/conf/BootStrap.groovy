8792e55c1ef02869c14f10b71101d7a1c645d9cb
Maurizio Turatti <maurizio.turatti@gmail.com>
2013-01-25 17:42:20 +0100

    Added Mongoable abstract class

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,15 +1,22 @@
 import hello.Person
 import com.gmongo.GMongo
+import org.bson.types.ObjectId
+import grails.converters.JSON
 
 class BootStrap {
 
     def init = { servletContext ->
+        
+        JSON.registerObjectMarshaller(ObjectId) {
+            it.toString()
+        }
+        
         environments {
             development {
                 // drop database
                 new GMongo().getDB('hello').dropDatabase()
 
-                new Person(firstName: "Mauro", lastName: "Rossi").save(failOnError: true)
+                new Person(firstName: "Marco", lastName: "Rossi").save(failOnError: true)
                 new Person(firstName: "Giuseppe", lastName: "Verdi").save(failOnError: true)
                 new Person(firstName: "Anna", lastName: "Bianchi").save(failOnError: true)
             }
